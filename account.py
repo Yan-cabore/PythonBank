55a868f61e312d31358b268e04b49bdf9b256629
Yan <carlosyan2402@gmail.com>
2026-06-04 00:02:52 -0300

    fix: identação da classe account e portfolio corrigidas

--- account.py
+++ account.py
@@ -2,9 +2,15 @@ from currency import Currency
 from portfolio import Investment, Portfolio
 from abc import ABC, abstractmethod
 
+
 class Account(ABC):
-    def __init__(self, account_id: int, holder: str, balance: float, currency: Currency):
-        
+    def __init__(
+        self,
+        account_id: int,
+        holder: str,
+        balance: float,
+        currency: Currency
+    ):
         self.account_id = account_id
         self.holder = holder
         self.balance = balance
@@ -15,55 +21,76 @@ class Account(ABC):
         pass
 
     def deposit(self, amount):
-        self.balance+=amount
+        self.balance += amount
         return self.balance
-    
+
     def check_balance(self):
-        return f"{self.balance}{self.currency.code}" 
+        return f"{self.balance}{self.currency.code}"
+
 
 class CheckingAccount(Account):
-   
-    def __init__(self, account_id, holder, balance, currency, overdraft_limit: float = -1000):
+    def __init__(
+        self,
+        account_id,
+        holder,
+        balance,
+        currency,
+        overdraft_limit: float = -1000
+    ):
         super().__init__(account_id, holder, balance, currency)
-
         self.overdraft_limit = overdraft_limit
-    
+
     def withdraw(self, amount):
         if (self.balance - amount) < self.overdraft_limit:
-            return f"Error: withdraw limit reached."
+            return "Error: withdraw limit reached."
+
         self.balance -= amount
         return f"You withdrew {amount}\nCurrent balance: {self.balance}"
 
+
 class SavingsAccount(Account):
-    def __init__(self, account_id, holder, balance, currency, interest_rate: float):
+    def __init__(
+        self,
+        account_id,
+        holder,
+        balance,
+        currency,
+        interest_rate: float
+    ):
         super().__init__(account_id, holder, balance, currency)
         self._interest_rate = interest_rate
-    
+
     def update_balance(self):
         self.balance *= (1 + self._interest_rate)
-        return self.balance
+        return f"Your current savings {self.balance}"
 
     def withdraw(self, amount):
         if (self.balance - amount) < 0:
-            return f"Your balance is currently zero"
+            return "Your balance is currently zero"
+
         self.balance -= amount
         return f"You withdrew {amount}\nCurrent balance: {self.balance}"
+
 
 class InvestmentAccount(Account):
     def __init__(self, account_id, holder, balance, currency):
         super().__init__(account_id, holder, balance, currency)
         self._portfolio = Portfolio()
 
-    def invest(self,name,amount,return_rate,date):
+    def invest(self, name, amount, return_rate, date):
         if self.balance - amount < 0:
             return "Error: not enough balance."
-        self.balance -= amount
-        self._portfolio.add_investment(Investment(name, amount, return_rate, date))
 
-        return f"Your investment has been successfull."
-    
+        self.balance -= amount
+        self._portfolio.add_investment(
+            Investment(name, amount, return_rate, date)
+        )
+
+        return "Your investment has been successfull."
+
     def withdraw(self, amount):
         if self.balance - amount < 0:
-            return f"Error: not enough balance."
+            return "Error: not enough balance."
+
         self.balance -= amount
-        return f"Your investment has been successfull."
+        return "Your investment has been successfull."
